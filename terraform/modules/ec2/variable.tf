84d308265b7cce6fe1e9d9f9b93cee03f0ef39c2
Darshan Dahake <dahakedarshan99@gmail.com>
2025-06-25 21:15:09 +0530

    created terraform more

---
+++ terraform/modules/ec2/variable.tf
@@ -0,0 +1,6 @@
+variable "region" {}
+variable "project" {}
+variable "instance_type" { default = "t3.micro" }
+variable "account_id" {}
+variable "ecr_repo_url" {}
+variable "instance_profile_name" {}
